ce9695781919c752cedb7e92fff6a36b8151d6ff
Shanha_Kim <Shanha_Kim@DESKTOP-RE70RQI>
2020-01-03 16:22:21 +0900

    김산하 20.01.03 크롤링기능 추가

---
+++ MusiColor/src/main/java/com/musicolor/www/services/Crawling.java
@@ -0,0 +1,36 @@
+package com.musicolor.www.services;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class Crawling {
+	public void crawl() {
+		try {
+			//웹에서 내용을 가져온다.
+			Document doc = Jsoup.connect("https://www.billboard.com/charts/hot-100/").get();
+			
+			//내용 중에서 원하는 부분을 가져온다.
+			Elements content_song = doc.select(".chart-element__information__song");
+			Elements content_vocal = doc.select(".chart-element__information__artist");
+			//원하는 부분은 Elements형태로 되어 있으므로 이를 String 형태로 바꾸어 준다.
+			System.out.println("##################여기는 음악제목################3 ");
+			List<String> songlist = content_song.eachText();
+			for(String x : songlist) {
+				System.out.println(x);
+			}
+			System.out.println(" ");
+			System.out.println("##################여기는 가수이름################3 ");
+			List<String> vocallist = content_vocal.eachText();
+			for(String x : vocallist) {
+				System.out.println(x);
+			}
+
+		} catch (IOException e) { //Jsoup의 connect 부분에서 IOException 오류가 날 수 있으므로 사용한다.   
+			e.printStackTrace();
+		}
+	}
+}
