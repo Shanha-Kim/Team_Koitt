466bfff7f1df6b42f7b88df58144ca42e85405fe
eunbindev <eunbin.dev@gmail.com>
2020-01-07 11:40:33 +0900

    박은빈 2020.01.07 ComtVO 수정

--- MusiColor/src/main/java/com/musicolor/www/vo/ComtVO.java
+++ MusiColor/src/main/java/com/musicolor/www/vo/ComtVO.java
@@ -12,7 +12,12 @@ public class ComtVO {
 	private char c_isshow;
 	private String c_body;
 	
+	private int ban;
 	
+	private int m_no;
+	private String id;
+	
+	private long r_no;
 	
 	public String getC_upid() {
 		return c_upid;
@@ -74,7 +79,28 @@ public class ComtVO {
 	public void setC_body(String c_body) {
 		this.c_body = c_body;
 	}
-	
-	
-	
+	public int getBan() {
+		return ban;
+	}
+	public void setBan(int ban) {
+		this.ban = ban;
+	}
+	public int getM_no() {
+		return m_no;
+	}
+	public void setM_no(int m_no) {
+		this.m_no = m_no;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public long getR_no() {
+		return r_no;
+	}
+	public void setR_no(long r_no) {
+		this.r_no = r_no;
+	}
 }
